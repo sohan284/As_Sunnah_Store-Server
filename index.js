80e6fb8219822b8fb79fe5dc21499c2c10e0a4a6
srsohan284 <srsohan284.com>
2022-08-04 19:42:52 +0600

    update

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p8oa6.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
 
 async function run(){
     try{
         await client.connect();
         const productCollection = client.db('as_sunnah').collection('products');
 
-        app.get('/products',async(req,res)=>{
+        app.get('/product',async(req,res)=>{
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
