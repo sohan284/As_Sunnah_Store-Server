5d848c2c6f3f236ae6f707393ce51428c9f9861e
srsohan284 <srsohan284.com>
2022-10-27 22:45:32 +0600

    cart

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run(){
         await client.connect();
         const productCollection = client.db('as_sunnah').collection('products');
         const cartCollection = client.db('as_sunnah').collection('cart');
+        const userCollection = client.db('as_sunnah').collection('users');
 
         app.get('/product',async(req,res)=>{
             const query = {};
@@ -46,31 +47,61 @@ async function run(){
 
         })
         app.get('/cart',async (req,res)=>{
-            const query = {};
+            const query = {};  
             const cursor = cartCollection.find(query);
             const cart = await cursor.toArray();
             res.send(cart);
          })
-         app.get('/cart/:id',async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const product = await cartCollection.findOne(query);
-            res.send(product);
-        })
-         
-        app.post('/cart',async (req,res)=>{
+         app.post('/cart',async (req,res)=>{
             const newCart = req.body;
-            console.log(req.body);
             const result = await cartCollection.insertOne(newCart);
             res.send(result);
         })
-       
+         app.get('/cart/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: id};
+            const cart = await cartCollection.findOne(query);
+            console.log(cart)
+            res.send(cart);
+        })
+
         app.delete('/cart/:id',async (req,res)=>{
-            const id = req.params.id
-            const query = {_id: ObjectId(id)};
+            const _id = req.params.id
+            const query = {_id: _id};
             const result = await cartCollection.deleteOne(query);
+            console.log(result);
             res.send(result);
         })
+        app.get('/user',async (req,res)=>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const user = await cursor.toArray();
+            res.send(user);
+         })
+         app.post('/user',async (req,res)=>{
+             const newUser = req.body;
+             console.log('request',newUser)
+             const result = await userCollection.insertOne(newUser);      
+             res.send(result)
+         })
+         app.delete('/user/:id',async (req,res)=>{
+             const id = req.params.id
+             const query = {_id: ObjectId(id)};
+             const result = await userCollection.deleteOne(query);
+             res.send(result);
+         })
+         app.get('/user/:id',async (req,res)=>{
+             const id = req.params.id
+             const query = {_id: ObjectId(id)};
+             const result = await userCollection.findOne(query);
+             res.send(result);
+         })
+         app.get('/user/:id', (req,res)=>{
+             console.log(req.params.id);
+             res.send('i am papas id')
+         })
+         
+ 
 
     }
     finally{
