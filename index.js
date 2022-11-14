102205b5e9cf272326e2b8a0827225de2426cab6
srsohan284 <srsohan284.com>
2022-11-14 19:25:11 +0600

    admin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p8oa6.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorised access'});
+    }
+}
 
 async function run(){
     try{
@@ -70,23 +78,23 @@ async function run(){
             res.send(result);
         })
        
-        app.get('/user',async (req,res)=>{
-            const query = {};
-            const cursor = userCollection.find(query);
-            const user = await cursor.toArray();
-            res.send(user);
+        app.get('/user',verifyJWT,async (req,res)=>{
+            const users = await userCollection.find().toArray();
+            res.send(users);
          })
          app.put('/user/:email',async(req,res)=>{
             const email = req.params.email;
-            const filter = {email: email};
             const user = req.body;
+            const filter = {email: email};
             const options = {upsert: true};
-            console.log(user);
             const updateDoc = {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter,updateDoc,options);
-            res.send(result);
+            const token = jwt.sign({email : email},process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+            console.log(token)
+            const value ={result,token}
+            res.send(value);
         })
 
     }
