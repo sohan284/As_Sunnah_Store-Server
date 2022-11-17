a282b484f9f589469cb2b99d6ddf132fd78aa608
srsohan284 <srsohan284.com>
2022-11-18 00:13:10 +0600

    admin

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req,res,next){
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: 'UnAuthorised access'});
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
     }
-}
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 async function run(){
     try{
@@ -27,13 +35,21 @@ async function run(){
         const productCollection = client.db('as_sunnah').collection('products');
         const cartCollection = client.db('as_sunnah').collection('cart');
         const userCollection = client.db('as_sunnah').collection('users');
-
+  
+  
+        // ****************   Product  ***************
         app.get('/product',async(req,res)=>{
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         })
+        app.post('/product',async(req,res)=>{
+            const newProduct = req.body;
+            console.log(newProduct)
+            const result = await productCollection.insertOne(newProduct);
+            // res.send(result);
+        });
         app.get('/product/:id',async(req,res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
@@ -53,6 +69,8 @@ async function run(){
             const result = await productCollection.updateOne(filter,updatedDoc,options);
             res.send(result);
         })
+
+   // ****************   Cart  ***************
         app.get('/cart',async (req,res)=>{
             const query = {};  
             const cursor = cartCollection.find(query);
@@ -76,12 +94,22 @@ async function run(){
             const query = {_id: _id};
             const result = await cartCollection.deleteOne(query);
             res.send(result);
-        })
-       
+        })  
+        
+        
+   // ****************   User ***************
         app.get('/user',verifyJWT,async (req,res)=>{
             const users = await userCollection.find().toArray();
             res.send(users);
          })
+         app.delete('/user/:id',async (req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)};
+            console.log(query)
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        }) 
+
          app.put('/user/:email',async(req,res)=>{
             const email = req.params.email;
             const user = req.body;
@@ -92,11 +120,34 @@ async function run(){
             };
             const result = await userCollection.updateOne(filter,updateDoc,options);
             const token = jwt.sign({email : email},process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
-            console.log(token)
             const value ={result,token}
             res.send(value);
         })
 
+         // ****************    Admin  ***************
+        app.get('/admin/:email',async(req,res)=>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+         })
+         app.put('/user/admin/:email',verifyJWT,async(req,res)=>{
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({email: requester});
+            if(requesterAccount.role === 'admin'){
+                const filter = {email: email};
+                const updateDoc = {
+                    $set: {role:'admin'},
+                };
+                const result = await userCollection.updateOne(filter,updateDoc);
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'forbidden'});
+            }
+          
+        })
     }
     finally{
 
